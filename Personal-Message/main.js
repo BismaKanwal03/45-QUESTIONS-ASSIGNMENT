72e410d207d911962688eb62f25ce046b9cf182f
BismaKanwal03 <bismakanwal2005@gmail.com>
2024-10-02 19:43:33 +0500

    Add files via upload

---
+++ Personal-Message/main.js
@@ -0,0 +1,16 @@
+// 45 QUESTION ASIGNMENT
+// QUESTION NUMBER ; 02
+//PERSONAL MESSAGE
+
+//storing a persons name in a variable
+
+var personName = "Bisma Imran";
+
+//Storing the message in a variable
+
+var message =
+  "Hello! I'm a passionate student of Computer Science. I am learning Web-Development at GIAIC";
+
+// Printing the message using template literals
+
+console.log("Hi dear! ".concat(personName, " ").concat(message, "."));
---
+++ Personal-Message/main.ts
@@ -0,0 +1,16 @@
+// 45 QUESTION ASIGNMENT
+
+// QUESTION NUMBER ; 02
+//"PERSONAL MESSAGE"
+
+//storing a persons name in a variable
+
+let personName: string = "Bisma Imran";
+
+//Storing the message in a variable
+
+let message: string = "Hello! I'm a passionate student of Computer Science. I am learning Web-Development at GIAIC";
+
+// Printing the message using template literals
+
+console.log(`Hi dear! ${personName} ${message}.`);

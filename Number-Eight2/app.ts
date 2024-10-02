69c610fe4e6b235829465a876ebc8f2080e94dd8
BismaKanwal03 <bismakanwal2005@gmail.com>
2024-10-02 20:09:47 +0500

    Add files via upload

---
+++ Number-Eight2/app.ts
@@ -0,0 +1,10 @@
+// 45 QUESTIONS ASSIGNMENT
+
+//Q 8
+
+//NUMBER 08 APPEARING ONCE ON EACH LINE
+
+console.log(3 + 5); //OUTPUT => 8
+console.log(12 - 4); //OUTPUT => 8
+console.log(7 + 1); //OUTPUT => 8
+console.log(4 * 2); //OUTPUT => 8

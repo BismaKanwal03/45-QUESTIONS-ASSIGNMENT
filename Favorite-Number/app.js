9f57122db83790231cf2adc912d4b6b399bf869f
BismaKanwal03 <bismakanwal2005@gmail.com>
2024-10-02 20:13:25 +0500

    Add files via upload

---
+++ Favorite-Number/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// 45 QUESTIONS ASSIGNMENT
+
+// Q 09
+
+// "FAVORITE NUMBER"
+
+//Storing my favotite number in a variable
+let favNum = 3;
+
+//printing my favoritenumber with a message
+let message = "is my favorite number.";
+console.log(`${favNum} => ${message}`);
---
+++ Favorite-Number/app.ts
@@ -0,0 +1,12 @@
+// 45 QUESTIONS ASSIGNMENT
+
+// Q 09
+
+// FAVORITE NUMBER
+
+//Storing my favotite number in a variable
+let favNum:number=3
+
+//printing my favoritenumber with a message
+let message : string="is my favorite number."
+console.log(`${favNum} => ${message}`)

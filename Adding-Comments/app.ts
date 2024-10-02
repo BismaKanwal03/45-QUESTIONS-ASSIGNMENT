71bd5d600b5dbeae170c1fe1f224676f29e7a641
BismaKanwal03 <bismakanwal2005@gmail.com>
2024-10-02 20:16:05 +0500

    Add files via upload

---
+++ Adding-Comments/app.ts
@@ -0,0 +1,18 @@
+// 45 QUESTIONS ASSIGNMENT 
+
+// Q 10
+
+//ADDING COMMENTS
+
+// the line i have written on the top are called single line comment
+let singleComment= "the line above"
+ 
+/* & what im doing now is called multiple line comments,
+it starts with a `/` along with `*` on the frist line 
+
+let multipleComment = "these specific lines"
+
+& it ends with a `*` along with a `/` 
+
+*/
+ let MultipleComment = "the lines above"

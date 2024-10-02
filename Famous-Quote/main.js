d9f5b10a78cf0c4d518b0ef2757bdafe03e97a05
BismaKanwal03 <bismakanwal2005@gmail.com>
2024-10-02 19:51:43 +0500

    Add files via upload

---
+++ Famous-Quote/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//45 QUESTION ASSIGNMENT
+//Q 04
+//FAMOUS QUOTES
+
+//Adding the Quote in a Variable
+let famousQuote = "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.";
+// printing the quote
+
+console.log(`Oprah Winfrey once said : "${famousQuote}"`);
---
+++ Famous-Quote/main.ts
@@ -0,0 +1,12 @@
+//45 QUESTION ASSIGNMENT
+
+//Q 04
+//FAMOUS QUOTES
+
+//Adding the quote in a Variable
+
+let famousQuote :string = "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough."
+
+// printing the quote
+
+console.log(`Oprah Winfrey once said : "${famousQuote}"`)

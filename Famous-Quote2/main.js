f149e880c2470e56e1212b1733f31fa22c0fda50
BismaKanwal03 <bismakanwal2005@gmail.com>
2024-10-02 20:01:04 +0500

    Add files via upload

---
+++ Famous-Quote2/main.js
@@ -0,0 +1,11 @@
+"use strict";
+//45 QUESTION ASSIGNMENT
+//Q 05
+//FAMOUS QUOTE 2
+// storing famous persons name in a variable
+let famous_person = "James Cameron";
+//storing the famous quote in a variable
+let message =
+  "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.";
+//printing the message
+console.log(`${famous_person}:"${message}"`);
---
+++ Famous-Quote2/main.ts
@@ -0,0 +1,14 @@
+//45 QUESTION ASSIGNMENT
+
+//Q 05
+//FAMOUS QUOTE 2
+
+// storing famous persons name in a variable
+let famous_person: string = "James Cameron";
+
+//storing the famous quote in a variable
+let message: string =
+  "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.";
+
+//printing the message
+console.log(`${famous_person}:"${message}"`);

89d3af0cc1c11a28b78b2e3499a526c2c025da80
BismaKanwal03 <bismakanwal2005@gmail.com>
2024-10-02 20:03:58 +0500

    Add files via upload

---
+++ Stripping-Names/main.ts
@@ -0,0 +1,17 @@
+//45 QUESTIONS ASSIGNMENT
+
+//QUESTION NUMBER 11
+
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+//Storing friends names in a variable.
+
+let names: string[] = ["narmeen", "zainab", "misbah", "alisha", "seher"];
+
+//using a for loop to print the names one by one
+
+for (let i = 0; i < names.length; i++) {
+  //printing the names
+
+  console.log(names[i]);
+}

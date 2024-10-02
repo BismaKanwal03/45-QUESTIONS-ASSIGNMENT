88d164a91d1a29ac5d17edefcf79b8f28111bcd1
BismaKanwal03 <bismakanwal2005@gmail.com>
2024-10-02 20:08:15 +0500

    Add files via upload

---
+++ Number-Eight/app.ts
@@ -0,0 +1,17 @@
+// 45 questions assignment
+
+// Q 07
+
+// "NUMBER 08"
+
+//printing "8" using addition operator
+console.log(5 + 3); //addition => output = 8
+
+//printing "8" using subtraction operator
+console.log(10 - 2); //subtraction => output = 8
+
+//printing "8" using division operator
+console.log(32 / 4); //division => output = 8
+
+//printing 8 using multiplication operator
+console.log(4 * 2); //multiplication => output = 8
